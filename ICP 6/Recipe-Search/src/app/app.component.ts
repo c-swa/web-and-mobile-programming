647985e93e1b2b89e2efbe5b81d64746ee57cfab
Christopher Swayne <chris_swa@icloud.com>
2020-10-14 17:02:12 -0500

    Updated ICP 6
    Updated Restaurant Search to provide a better Address on the search page.
    Updated Recipe Search to use better font naming-schemes.

--- ICP 6/Recipe-Search/src/app/app.component.ts
+++ ICP 6/Recipe-Search/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -19,29 +19,30 @@ export class AppComponent {
   hits = [];
 
   // API keys/necessary bits:
-  app_id = "dd7d20b9";
-  app_key = "eaf4b5b351249770ee7d65b7f434ab20";
+  appId = 'dd7d20b9';
+  appKey = 'eaf4b5b351249770ee7d65b7f434ab20';
   // Search Result indices
   start = '0';
   end = '10';
 
+  // tslint:disable-next-line:typedef
   search(){
     // Generate URL to make request to.
-    let search_url = 'https://api.edamam.com/search';
-    search_url += "?q=" + this.recipe;
-    search_url += "&app_id=" + this.app_id;
-    search_url += "&app_key=" +this.app_key;
-    search_url += "&from=" + this.start;
-    search_url += "&to=" +this.end;
+    let searchUrl = 'https://api.edamam.com/search';
+    searchUrl += '?q=' + this.recipe;
+    searchUrl += '&app_id=' + this.appId;
+    searchUrl += '&app_key=' + this.appKey;
+    searchUrl += '&from=' + this.start;
+    searchUrl += '&to=' + this.end;
 
     // output URL for testing
-    console.log(search_url);
+    console.log(searchUrl);
 
     // Make Request
-    this.httpClient.get<any>(search_url).subscribe(data=>{
+    this.httpClient.get<any>(searchUrl).subscribe(data => {
       console.log(data.hits);
       this.hits = data.hits;
       console.log(this.hits);
-    })
+    });
   }
 }
