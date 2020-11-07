d3be547f710aaae453710cd54d4fc77f7c4b4622
Christopher Swayne <chris_swa@icloud.com>
2020-11-06 20:17:09 -0600

    Built Login Page
    Built the Login Page - successfully pulled data from EditTexts' and managed to get the Submit button to work
    
    - Current issues:
    Can't move to other Intent

--- ICP 8/Mobile_Lesson1/HelloWorldApp/app/src/main/java/com/csee5590/helloworldapp/MainActivity.java
+++ ICP 8/Mobile_Lesson1/HelloWorldApp/app/src/main/java/com/csee5590/helloworldapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.csee5590.helloworldapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final EditText usernameCtrl = (EditText) findViewById(R.id.usernameEdit);
+        final EditText passwordCtrl = (EditText) findViewById(R.id.passwordEdit);
+
+        final Button buttonSubmit = (Button) findViewById(R.id.submit);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usernameText = usernameCtrl.getText().toString();
+                String passwordText = passwordCtrl.getText().toString();
+
+                if (Submit(usernameText, passwordText) == 0){
+                    Intent redirect = new Intent(MainActivity.this, WelcomeActivity.class);
+                    startActivity(redirect);
+                }
+            }
+        });
+    }
+
+    public int Submit(String usernameText, String passwordText){
+        if (onSubmit(usernameText, passwordText)){
+            return 0;
+        } else {
+            return 100;
+        }
+    }
+
+    private boolean onSubmit(String usernameText, String passwordText){
+        if (!usernameText.isEmpty() && !passwordText.isEmpty()){
+            if (usernameText.equals("Admin") && passwordText.equals("Admin")){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
--- ICP 8/Mobile_Lesson1/HelloWorldApp/app/src/main/java/com/csee5590/helloworldapp/WelcomeActivity.java
+++ ICP 8/Mobile_Lesson1/HelloWorldApp/app/src/main/java/com/csee5590/helloworldapp/WelcomeActivity.java
@@ -1,4 +1,14 @@
 package com.csee5590.helloworldapp;
 
-public class WelcomeActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
 }
