360f688b0150b6e4c29e4945a2c014cbb8a51373
Christopher Swayne <chris_swa@icloud.com>
2020-11-07 17:51:36 -0600

    ICP 8 Finalized
    ICP has been completed, the login page has been complete.

--- ICP 8/Mobile_Lesson1/HelloWorldApp/app/src/main/java/com/csee5590/helloworldapp/WelcomeActivity.java
+++ ICP 8/Mobile_Lesson1/HelloWorldApp/app/src/main/java/com/csee5590/helloworldapp/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package com.csee5590.helloworldapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        Button logoutButton = (Button) findViewById(R.id.logout_button);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                try {
+                    Intent redirect = new Intent(WelcomeActivity.this, MainActivity.class);
+                    startActivity(redirect);
+                } catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+        });
     }
 }
