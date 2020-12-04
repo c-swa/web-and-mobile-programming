f43eb9289bbea25f077451c58cef8449e4d612bb
Christopher Aram Swayne <42748779+c-swa@users.noreply.github.com>
2020-12-03 22:53:01 -0600

    updated text to speech

--- ICP-13/Source/Mobile_Lesson6/MedicalAssistant/app/src/main/java/com/example/medicalassistant/MainActivity.java
+++ ICP-13/Source/Mobile_Lesson6/MedicalAssistant/app/src/main/java/com/example/medicalassistant/MainActivity.java
@@ -18,7 +18,6 @@ import java.util.Locale;
 
 // Text-to-Speech Components
 import android.speech.tts.TextToSpeech;
-import android.net.Uri;
 
 
 
@@ -35,16 +34,13 @@ public class MainActivity extends AppCompatActivity {
     private final int CHECK_CODE = 0x1;
     private final int LONG_DURATION = 5000;
     private final int SHORT_DURATION = 1500;
-    TextToSpeech textToSpeech;
+    TextToSpeech speaker;
 
     // Answers
     private final String I_CAN_UNDERSTAND = "I can understand, Please tell us your symptoms in short.";
     private final String THANK_YOU = "Thank you too ";
     private final String TAKE_CARE = " take care.";
-    private final String CURRENT_TIME = "The current time is ";
     private final String MEDICINE = "I think you have a fever, please take this medicine.";
-
-    private Speaker speaker;
 
 
     // -----------------------------------------------------------------
@@ -71,12 +67,12 @@ public class MainActivity extends AppCompatActivity {
         editor = preferences.edit();
 
         // Text-to-Speech
-        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        speaker = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if(status < 0){
-                    textToSpeech.setLanguage(Locale.getDefault());
-                    textToSpeech.speak("Welcome",TextToSpeech.QUEUE_FLUSH, null);
+                    speaker.setLanguage(Locale.getDefault());
+                    speaker.speak("Welcome",TextToSpeech.QUEUE_FLUSH, null);
                 }
             }
         });
@@ -108,38 +104,38 @@ public class MainActivity extends AppCompatActivity {
                     if (res.size() > 0) {
                         // Asks user's name after preliminary "hello"
                         if (res.get(0).equalsIgnoreCase("HELLO")) {
-                            textToSpeech.speak("What is your name?", TextToSpeech.QUEUE_FLUSH, null);
+                            speaker.speak("What is your name?", TextToSpeech.QUEUE_FLUSH, null);
                             mVoiceInputTv.append("\nWhat is your name?");
                         }
                         // Retrieves user's name.
                         else if (res.get(0).contains("name")) {
                             editor.putString("NAME", res.get(0).substring(res.get(0).lastIndexOf(" ") + 1)).apply();
-                            textToSpeech.speak(("Nice to meet you " + preferences.getString("NAME", "")), TextToSpeech.QUEUE_FLUSH, null);
+                            speaker.speak(("Nice to meet you " + preferences.getString("NAME", "")), TextToSpeech.QUEUE_FLUSH, null);
                             mVoiceInputTv.append("\n" + "Nice to meet you " + preferences.getString("NAME", ""));
                         }
                         // Returns "I can understand. Please tell me your symptoms in short" if user says "I'm not feeling well"
                         else if (res.get(0).contains("not feeling well")) {
-                            textToSpeech.speak(I_CAN_UNDERSTAND, TextToSpeech.QUEUE_FLUSH, null);
+                            speaker.speak(I_CAN_UNDERSTAND, TextToSpeech.QUEUE_FLUSH, null);
                             mVoiceInputTv.append("\n" + I_CAN_UNDERSTAND);
                         }
                         // Says "thank you" if user also says "thank you"
                         else if (res.get(0).contains("thank")) {
-                            textToSpeech.speak(THANK_YOU + preferences.getString("NAME", "") + TAKE_CARE, TextToSpeech.QUEUE_FLUSH, null);
+                            speaker.speak(THANK_YOU + preferences.getString("NAME", "") + TAKE_CARE, TextToSpeech.QUEUE_FLUSH, null);
                             mVoiceInputTv.append("\n" + THANK_YOU + preferences.getString("NAME", "") + TAKE_CARE);
                         }
                         // Returns the time
                         else if (res.get(0).contains("time")) {
-                            textToSpeech.speak("The time is " + getCurrentTime(), TextToSpeech.QUEUE_FLUSH, null);
+                            speaker.speak("The time is " + getCurrentTime(), TextToSpeech.QUEUE_FLUSH, null);
                             mVoiceInputTv.append("\n" + "The time is " + getCurrentTime());
                         }
                         // Suggests medicine
                         else if (res.get(0).contains("medicines")) {
-                            textToSpeech.speak(MEDICINE, TextToSpeech.QUEUE_FLUSH, null);
+                            speaker.speak(MEDICINE, TextToSpeech.QUEUE_FLUSH, null);
                             mVoiceInputTv.append("\n" + MEDICINE);
                         }
                         // If any issues or spoken words did not make sense to speech interpreter
                         else {
-                            textToSpeech.speak("I didn't understand you, can you say that again?", TextToSpeech.QUEUE_FLUSH, null);
+                            speaker.speak("I didn't understand you, can you say that again?", TextToSpeech.QUEUE_FLUSH, null);
                             mVoiceInputTv.append("\n" + "Please say that again, I didn't understand you.");
                         }
                     }
@@ -157,13 +153,5 @@ public class MainActivity extends AppCompatActivity {
             strDate[1] = "o'clock";
         }
         return sdf.format(now);
-    }
-
-    // Below is the Text-to-Speech Component
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        speaker.destroy();
     }
 }
